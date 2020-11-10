1d2bda4b5d8886a15895715644b797b4c82aefe7
Adrian Barabino <adrian.barabino@gmail.com>
2020-11-10 19:13:51 -0300

    Now fixed navbar change class and stylish when scroll down

--- static/front/assets/js/main.js
+++ static/front/assets/js/main.js
@@ -1,5 +1,18 @@
 // $(document).ready(function () {
 
+	$(function() {
+		var header = $(".navbar");
+	  
+		$(window).scroll(function() {    
+			var scroll = $(window).scrollTop();
+			if (scroll >= 100) {
+				header.addClass("scrolled");
+			} else {
+				header.removeClass("scrolled");
+			}
+		});
+	  
+	});
 
 	$("#menuModal .nav-link").click(function(){
 		
